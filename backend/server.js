b51aa2d59558e9c1302235f8a82bb016beb3ee26
Tapish1511 <87308840+Tapish1511@users.noreply.github.com>
2023-03-29 17:33:56 +0530

    data is stored in mongo

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const path = require('path')
+const path = require('path');
+require('dotenv').config();
+const searchSong = require('./routers/searchSong')
+
 
 const app = express();
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@${process.env.CLUSTER}.fztb6pi.mongodb.net/?retryWrites=true&w=majority`
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+app.use(cors());
+app.use(express.json());
+
+async function connectToDB(){
+    mongoose.connect(uri).then(()=>{
+        console.log("connected")
+    });
+}
+connectToDB();
 
 
-app.get('/', (req, res)=>{
-    res.send("hello world");
-});
+app.use('/searchSong', searchSong);
 
 app.listen(5000, ()=>{
     console.log("api started");
