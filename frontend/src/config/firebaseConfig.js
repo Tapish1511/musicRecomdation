3ed02374022610691e95b69e34bd526b971f1e90
Tapish1511 <87308840+Tapish1511@users.noreply.github.com>
2023-03-29 14:08:22 +0530

    firebase config added

---
+++ frontend/src/config/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCKHZigsKxQDNSDnIUurceWppMKeleRvAA",
+  authDomain: "music-recommendation-4eb89.firebaseapp.com",
+  projectId: "music-recommendation-4eb89",
+  storageBucket: "music-recommendation-4eb89.appspot.com",
+  messagingSenderId: "358928939562",
+  appId: "1:358928939562:web:8f16edc9b5d8ee7de32153"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
