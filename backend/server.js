25a4a9fa30538cd83497bd3dc74b0a3eb9a005f8
Tapish1511 <87308840+Tapish1511@users.noreply.github.com>
2023-04-13 11:23:53 +0530

    minor issues are fixed

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,18 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
 require('dotenv').config();
-const searchSong = require('./routers/searchSong')
+const searchSong = require('./routers/searchSong');
+
 
 
 const app = express();
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@${process.env.CLUSTER}.fztb6pi.mongodb.net/?retryWrites=true&w=majority`
 
+app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.resolve(__dirname, "../frontend/build")));
-app.use(cors());
 app.use(express.json());
+
 
 async function connectToDB(){
     mongoose.connect(uri).then(()=>{
@@ -23,6 +25,9 @@ connectToDB();
 
 
 app.use('/searchSong', searchSong);
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+});
 
 app.listen(5000, ()=>{
     console.log("api started");
