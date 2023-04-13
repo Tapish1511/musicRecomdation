45e6e9f6fcc4a4b2809847fcf952ebd23d8c365c
Tapish1511 <87308840+Tapish1511@users.noreply.github.com>
2023-04-13 19:44:31 +0530

    navbar structure added

---
+++ frontend/src/pages/navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+function NavBar(){
+
+    return(
+        <>
+            <nav className={styles.navContainer}>
+                <div className={styles.Name}>
+                    <h1>Music Recommendation</h1>
+                </div>
+                <div className={styles.navItems}>
+                    <ul className={styles.navItemList}>
+                        <li><Link to='/about'>About us</Link></li>
+                        <li><a href="https://github.com/Tapish1511/musicRecomdation" target="_blank">Code</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default NavBar
